2629225c03ed627bc55b3ac14eaf48e6ee018f91
Jeffcc <jeff95001@gmail.com>
2020-12-10 19:15:53 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <cstring>
+
+using namespace std;
+
+void maxx(int, int*);
+
+int main()
+{
+    int arr[3];
+
+    for(int i = 0; i < 3; i++)
+    {
+        arr[i] = rand()%10+1;
+    }
+
+
+
+    return 0;
+}
+
+void maxx(int arr[3], int *maxx)
+{
+
+
+    return
+}
